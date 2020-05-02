193d31cefbe31e776d815dffc4ecb8e522d6309d
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-05-02 20:46:25 +0200

    feat: Update Banner

--- webpack.base.js
+++ webpack.base.js
@@ -9,15 +9,21 @@ const BANNER = stripIndent `
     // ==UserScript==
     // @name         YT Sync
     // @namespace    https://tandashi.de
-    // @version      0.1
-    // @description  try to take over the world!
+    // @version      1.0
+    // @description  Sync Youtube Videos directly on Youtube :)
     // @author       Tandashi
     // @match        https://www.youtube.com/*
     //
-    // @grant       GM_setValue
-    // @grant       GM_getValue
+    // @icon         http://www.genyoutube.net/helper/favicon.png
+    // @icon64       http://www.genyoutube.net/helper/favicon.png
+    // @homepage     https://tandashi.de/
+    // @downloadURL  https://github.com/Tandashi/YTSync-Plugin/releases/latest
+    // @updateURL    https://github.com/Tandashi/YTSync-Plugin/releases/latest
     //
-    // @run-at      document-end
+    // @grant        GM_setValue
+    // @grant        GM_getValue
+    //
+    // @run-at       document-end
     //
     // @require http://code.jquery.com/jquery-3.4.1.min.js
     // @require https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js
