0555875830916b9e38a9edd4984e87e0c0f503b8
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-05-03 21:38:41 +0200

    V1.2

--- webpack.base.js
+++ webpack.base.js
@@ -9,7 +9,7 @@ const BANNER = stripIndent `
     // ==UserScript==
     // @name         YT Sync
     // @namespace    https://tandashi.de
-    // @version      1.1
+    // @version      1.2
     // @description  Sync Youtube Videos directly on Youtube :)
     // @author       Tandashi
     // @match        https://www.youtube.com/*
