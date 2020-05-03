c277940041893e970341181e2e19b469f70f9690
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-05-03 22:12:39 +0200

    fix: production connection :/

--- src/plugin.ts
+++ src/plugin.ts
@@ -16,9 +16,9 @@ const intervals: PluginInjectIntervals = {
 
 const player = new Player({
     connection: {
-        protocol: 'http',
-        host: '127.0.0.1',
-        port: '8080'
+        protocol: 'https',
+        host: 'sync.tandashi.de',
+        port: '443'
     }
 });
 
