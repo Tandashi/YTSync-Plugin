ac7c3e16e66779ff4dc1675c006996a19c4393d7
Tandashi <18377875+Tandashi@users.noreply.github.com>
2020-05-06 19:04:48 +0200

    doc: Update version to 1.4

--- webpack.base.js
+++ webpack.base.js
@@ -9,7 +9,7 @@ const BANNER = stripIndent `
     // ==UserScript==
     // @name         YT Sync
     // @namespace    https://tandashi.de
-    // @version      1.3
+    // @version      1.4
     // @description  Sync Youtube Videos directly on Youtube :)
     // @author       Tandashi
     // @match        https://www.youtube.com/*
